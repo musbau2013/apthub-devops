bce94723261e7459854bf97b4c16e62cb982ef19
mtiamiyu <mtiamiyu@precocityllc.com>
2023-08-01 17:09:38 -0700

    workflows and config files

---
+++ infrastructure/environment/src/main.tf
@@ -0,0 +1,14 @@
+
+provider "aws" {
+  region = "us-east-1" # Change this to your desired AWS region
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-09538990a0c4fe9b" # Change this to your desired Amazon Machine Image (AMI)
+  instance_type = "t2.micro"             # Change this to your desired instance type
+  key_name      = "your_key_pair"   # Replace with the name of your existing EC2 key pair
+}
+
+output "public_ip" {
+  value = aws_instance.ec2_instance.public_ip
+}
