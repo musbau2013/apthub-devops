4e3aa9367df81385ffdfcf2d57950c29f5186992
mtiamiyu <mtiamiyu@precocityllc.com>
2023-08-13 12:32:41 -0700

    try again

--- infrastructure/environment/src/main.tf
+++ infrastructure/environment/src/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-09538990a0c4fe9b" # Change this to your desired Amazon Machine Image (AMI)
   instance_type = "t2.micro"             # Change this to your desired instance type
-  key_name      = "your_key_pair_name"  # Replace with the name of your existing EC2 key pair
+  key_name      = "your_key_pair_nam"  # Replace with the name of your existing EC2 key pair
 }
 
 output "public_ip" {
