ef56603e54899b0aec51213cc9fe7903a58910aa
mtiamiyu <mtiamiyu@precocityllc.com>
2023-08-13 13:30:00 -0700

    destroy workflow added

--- infrastructure/environment/src/main.tf
+++ infrastructure/environment/src/main.tf
@@ -6,8 +6,15 @@ provider "aws" {
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-08a52ddb321b32a8c" # Change this to your desired Amazon Machine Image (AMI)
   instance_type = "t2.micro"             # Change this to your desired instance type
-  # key_name      = "your_key_pair_nam"  # Replace with the name of your existing EC2 key pair
+
+  tags = {
+    Name = "MyEC2Instance"              # Example tag; you can replace this with your desired tag
+    Environment = "development"          # Another example tag
+  }
+
+  # key_name      = "your_key_pair_name" # Replace with the name of your existing EC2 key pair
 }
+
 
 output "public_ip" {
   value = aws_instance.ec2_instance.public_ip
